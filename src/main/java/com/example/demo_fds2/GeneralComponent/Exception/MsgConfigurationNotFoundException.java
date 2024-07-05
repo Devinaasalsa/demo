5e74c17aad354ee22478a2b274f3794ccc0520b3
Devinaasalsa <devinasdiva@gmail.com>
2024-07-05 15:32:26 +0700

    refactoring class

--- src/main/java/com/example/demo_fds2/GeneralComponent/Exception/MsgConfigurationNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo_fds2.GeneralComponent.Exception;
-
-public class MsgConfigurationNotFoundException extends Exception {
-    public MsgConfigurationNotFoundException() {
-        super("Msg Configuration not found, please input correct one.");
-    }
-}
--- src/main/java/com/example/demo_fds2/TransactionEnginee/Constant/StateType.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo_fds2.TransactionEnginee.Constant;
-
-public enum StateType {
-    REQUEST,
-    RESPONSE
-}
